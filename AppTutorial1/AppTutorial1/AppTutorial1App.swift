8c7d1da89efedafafe3a815bdfecbcf175b2d4ad
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-05 00:52:07 +0530

    basics of swift and swift ui V.1
    I learnt how to change the appearance of elements, without writing actual code. I could change the appearance of the element or add elements by clicking the '+' sign on the top right corner of the screen, and use the modifiers section. for example, if I wanted to change the background color, or the padding of the element, all I need to do is click the name of the modifier in the modifiers tab, and drag and drop the modifier into the code section.

---
+++ AppTutorial1/AppTutorial1/AppTutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppTutorial1App.swift
+//  AppTutorial1
+//
+//  Created by Vedant Mistry on 05/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AppTutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTutorial1/AppTutorial1/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppTutorial1
+//
+//  Created by Vedant Mistry on 05/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Placeholder")
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/).foregroundColor(Color.white).background(Color.orange)
+            .cornerRadius(10)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
